2aa5e0679b5896ed05be6f0773a76d923de85cba
Paul Nwodo <145398513+Tonypaul544@users.noreply.github.com>
2025-02-15 22:17:42 +0100

    Add files via upload

--- another.js
+++ another.js
@@ -1,13 +1,70 @@
 
 
+
+
+
+
+
+const nameDiv = document.getElementById("nameDiv");
+let profile = document.getElementById("profile-div");
+
+let profilePic = document.getElementById("blank");
+let typename = document.getElementById("typename");
+let inputFile = document.getElementById("writeName");
+let writeName = document.getElementById("writeName");
+let saveName = document.getElementById("saveName");
+// let body = document.getElementById("container");
+
+profile.addEventListener("click", editProfile);
+function editProfile() {
+  nameDiv.style.display = "block";
+}
+
+
+saveName.addEventListener("click", updateProfile);
+function updateProfile() {
+  if(typename.value === ""){
+    alert("Name can`t be blank!")
+  }
+  else {
+    userName.innerHTML = typename.value;
+    typename.value = "";
+    nameDiv.style.display = "none";
+    let warning = document.getElementById("popup")
+    warning.innerHTML = `Congrats ${typename.value} Profile saved successfully`
+    warning.style.display = "block";
+  
+    setTimeout(() => {
+      warning.style.display = "none"
+    }, 2500);
+  }
+}
+
+
+// inputFile.onchange() = function(){
+//   profilePic.src = URL.createObjectURL(inputFile.files[0])
+// };
+
+
+
+
+
+
+
 const linkInput = document.getElementById("input-box");
 const nameInput = document.getElementById("name-box");
 const addBtn = document.getElementById("addBtn");
 const listItem = document.getElementById("items");
 let listcontainer = document.getElementById("listcontainer");
+const link = document.getElementById("link");
+const projectName = document.getElementById("projectName");
+let userName = document.getElementById("userName");
 
 
-let addTask = () => {
+
+
+let addTask = (e) => {
+  e.preventDefault();
 
 
   if(linkInput.value === '' || nameInput.value === '') {
@@ -21,28 +78,49 @@ let addTask = () => {
   }
 
   else {
-    
+
+    // let data = {};
+
+    // let geteName = () => {
+    //   data["pjName"] = nameInput.value
+      
+    // }
+
+    link.innerHTML = linkInput.value;
+    projectName.textContent = nameInput.value;
+
     listItem.innerHTML += `
     
     <div class="line line1"  id="line">
       <li class="list" id="link">List 1</li>
       <button class="nameBtn btn1" id="projectName">Play</button>
-    </div>
+    </div>`;
+
+    nameInput.value = "";
+    linkInput.value = "";
     
-    `;
     listItem.style.display = "flex";
     listItem.style.flexDirection = "column";
     projectName.innerHTML = nameInput.value;
-    acceptData()
+    
+    // acceptData()
 
   }
   saveData()
 }
 
+projectName.addEventListener("click", openLink );
+
+function openLink() {
+  if (linkInput) { //Checks if the link is empty
+    window.open(link, '_blank');
+  }
+}
+
 addBtn.addEventListener("click", addTask);
 
-const link = document.getElementById("link").innerHTML = linkInput.value;
-var projectName = document.getElementById("projectName");
+// const link = document.getElementById("link").innerHTML = linkInput.value;
+// var projectName = document.getElementById("projectName");
 
 
 function saveData() {
